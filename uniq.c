31c3995ffdfb86810811ba98df9cc39d055c7468
lEE <li33@usf.edu>
2022-09-10 21:34:42 -0400

    uniq

--- uniq.c
+++ uniq.c
@@ -20,64 +20,60 @@ required as an argument) and writes the ltered data to the output le.*/
 
 
 /*functions headers*/
-void uniq(int fd, int coption, int ioption, int doption);
+void uniq(int fd, int coption, int ioption, int doption);/*main uniq function, has option c,i,d, and none*/
 char tolower(unsigned char ch);/*required by strcmpnc*/
 int strcmpnc(const char *p, const char *q);/*comaprison that case does not matter*/
-int getline(int fd, char *buf, int len);
-void inizarr(char *str, int size);
+int getline(int fd, char *buf, int len);/*get line*/
+void inizarr(char *str, int size);/*initialize the array with given size*/
 
 int 
 getline(int fd, char *str, int len)
 {
-    int i, ro;
-    char letter;
+  int i, ro;
+  char letter;
 
-    for(i = 0; i + 1 < len; )
-    {
-      ro = read(fd, &letter, 1);/*read in*/
-      if(ro < 1)
-        return -1;
-      str[i++] = letter;
-      if(letter == '\n' || letter == '\r')
-         break;
-    }
-    str[i] = '\0';/*add to the end*/
-    return strlen(str);
+  for(i = 0; i + 1 < len; ){
+    ro = read(fd, &letter, 1);/*read in*/
+    if(ro < 1)
+      return -1;
+    str[i++] = letter;
+    if(letter == '\n' || letter == '\r')
+      break;
+  }
+  str[i] = '\0';/*add to the end*/
+  return strlen(str);
 }
 
 void 
 inizarr(char *str, int size)
 {
-    int i;
+  int i;
 
-    for(i = 0; i < size; i++)
-        str[i] = '\0';
+  for(i = 0; i < size; i++)
+    str[i] = '\0';
 }
 
 char
 tolower(unsigned char ch) {
-    if (ch >= 'A' && ch <= 'Z')
-        ch = 'a' + (ch - 'A');
-    return ch;
+  if (ch >= 'A' && ch <= 'Z')
+    ch = 'a' + (ch - 'A');
+  return ch;
 }
 
-int
-strcmpnc(const char * str1, const char * str2)
+int strcmpnc(const char * str1, const char * str2)
 {
-    int cpflag = 0;
+  int cpflag = 0;
 
-    while ( *str1 || *str2)
-    {
-        cpflag = tolower((int)(*str1)) - tolower((int)(*str2));
-        if (cpflag != 0)
-        {
-            break;
-        }
-        str1++;
-        str2++;
+  while ( *str1 || *str2){
+    cpflag = tolower((int)(*str1)) - tolower((int)(*str2));
+    if (cpflag != 0){
+      break;
     }
+    str1++;
+    str2++;
+  }
 
-    return cpflag;
+  return cpflag;
 }
 
 void
@@ -92,90 +88,77 @@ uniq(int fd, int coption, int ioption, int doption)
 
   if(coption){
     if(getline(fd, str1, SIZE) <= 0)
-        return;
+      return;
     strcpy(str2, str1);
-    while(1)
-    {
-        while(strcmp(str1, str2) == 0)
-        {
-            inizarr(str1, SIZE);
-            count++;
-            if(getline(fd, str1, SIZE) <= 0)
-            {
-                flag = 1;
-                break;
-            }
+    while(1){
+      while(strcmp(str1, str2) == 0){
+        inizarr(str1, SIZE);
+        count++;
+        if(getline(fd, str1, SIZE) <= 0){
+          flag = 1;
+          break;
         }
-        if(flag)
-        {
-            printf(1, "%d %s", count, str2);
-            return;
-        }
+      }
+      if(flag){
         printf(1, "%d %s", count, str2);
-        count = 0;
-        strcpy(str2, str1);
+        return;
+      }
+      printf(1, "%d %s", count, str2);
+      count = 0;
+      strcpy(str2, str1);
     }
   }
   else if(ioption){
-    while(1)
-    {
+    while(1){
+      inizarr(str1, SIZE);
+      if(getline(fd, str1, SIZE) <= 0)
+          return;
+      while(strcmpnc(str1, str2) == 0){
         inizarr(str1, SIZE);
         if(getline(fd, str1, SIZE) <= 0)
-            return;
-        while(strcmpnc(str1, str2) == 0)
-        {
-            inizarr(str1, SIZE);
-            if(getline(fd, str1, SIZE) <= 0)
-                return;
-        }
-        printf(1, "%s", str1);
-        strcpy(str2, str1);
+          return;
+      }
+      printf(1, "%s", str1);
+      strcpy(str2, str1);
     }
   }
   else if(doption){
-    while(1)
-    {
-        inizarr(str1, SIZE);
-        if(getline(fd, str1, SIZE) <= 0)
-            return;
-        if(strcmp(str1, str2) != 0)
-            strcpy(str2, str1);
-        else
-        {
-            while(strcmp(str1, str2) == 0)
-            {
-                inizarr(str1, SIZE);
-                if(getline(fd, str1, SIZE) <= 0)
-                {
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag)
-            {
-                printf(1, "%s", str2);
-                return;
-            }
-            printf(1, "%s", str2);
-            strcpy(str2, str1);
+    while(1){
+      inizarr(str1, SIZE);
+      if(getline(fd, str1, SIZE) <= 0)
+        return;
+      if(strcmp(str1, str2) != 0)
+        strcpy(str2, str1);
+      else{
+        while(strcmp(str1, str2) == 0){
+          inizarr(str1, SIZE);
+          if(getline(fd, str1, SIZE) <= 0){
+            flag = 1;
+            break;
+          }
         }
+        if(flag){
+          printf(1, "%s", str2);
+          return;
+        }
+        printf(1, "%s", str2);
+        strcpy(str2, str1);
+      }
     }
 
   }
   else{
-    while(1)
-    {
+    while(1){
+      inizarr(str1, SIZE);
+      if(getline(fd, str1, SIZE) <= 0)
+        return;
+      while(strcmp(str1, str2) == 0){
         inizarr(str1, SIZE);
         if(getline(fd, str1, SIZE) <= 0)
-            return;
-        while(strcmp(str1, str2) == 0)
-        {
-            inizarr(str1, SIZE);
-            if(getline(fd, str1, SIZE) <= 0)
-                return;
-        }
-        printf(1, "%s", str1);
-        strcpy(str2, str1);
+          return;
+      }
+      printf(1, "%s", str1);
+      strcpy(str2, str1);
     }
   }
 }
@@ -188,38 +171,55 @@ main(int argc, char *argv[])
   int c = 0, i = 0, d = 0;
 
   if(argc <= 1){
-    uniq(0,c,i,d);
+    //printf(1,"Too less argument.\n");
+    uniq(0,c,i,d);/*so that it can take input from pipe*/
   }
   if(argc == 2){
+    /*with pipe*/
+    if(argv[1][0] == '-'){
+      switch(argv[1][1]){
+        case 'i':
+          i = 1;
+          break;
+        case 'd':
+          d = 1;
+          break;
+        case 'c':
+          c = 1;
+          break;
+        default:
+          printf(1,"Option not suppoted.\n");
+          exit();
+      }
+      uniq(0,c,i,d);
+    }
+      
     /*only filename*/ 
     if((fd = open(argv[1], 0)) < 0){
       printf(1, "uniq: cannot open %s\n", argv[1]);
       exit();
     }
-    printf(1,"Filename: %s\n", argv[1]);
     uniq(fd,c,i,d);
     close(fd);
   }
   if(argc == 3){
     /*has options*/
-    if(argv[1][0] == '-')
-      {
-          switch(argv[1][1])
-          {
-              case 'i':
-                  i = 1;
-                  break;
-              case 'd':
-                  d = 1;
-                  break;
-              case 'c':
-                  c = 1;
-                  break;
-              default:
-                  printf(1,"Option not suppoted.\n");
-                  exit();
-          }
+    if(argv[1][0] == '-'){
+      switch(argv[1][1]){
+        case 'i':
+          i = 1;
+          break;
+        case 'd':
+          d = 1;
+          break;
+        case 'c':
+          c = 1;
+          break;
+        default:
+          printf(1,"Option not suppoted.\n");
+          exit();
       }
+    }
     else{
       printf(1,"Two arguments were received, the option must goes front of the filename, the option should start with '-'.\n");
       exit();
@@ -229,7 +229,6 @@ main(int argc, char *argv[])
       printf(1, "uniq: cannot open %s\n", argv[2]);
       exit();
     }
-    printf(1,"Filename: %s\n", argv[2]);
     uniq(fd,c,i,d);
     close(fd);  
   }
